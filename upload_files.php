a53ebc43d9cd2abc9753f75b4e7adddc5baac977
ndepalma12 <nickolasdepalma@gmail.com>
2013-10-17 18:28:03 -0700

    Update upload_files.php

--- upload_files.php
+++ upload_files.php
@@ -1,39 +1,77 @@
+<body bgcolor = "#00FFFF">
+
+<h2> FILE ATTRIBUTES </h2>	
+<form method="post">
+DEPARTMENT:	
+<select name="Dept">
+	<option value="NA">...</option>
+	<option value="Development">Development</option>
+	<option value="Sales">Sales</option>
+	<option value="Human Resources">Human Resources</option>
+	<option value="Accounting">Accounting</option>
+	<option value="Information Technology">Information Technology</option>		
+</select>
+<br>
+
+ROLE:
+<select name="Role">
+	<option value="NA">...</option>
+	<option value="1">Intern</option>
+	<option value="2">Assistant</option>
+	<option value="3">Employee</option>
+	<option value="4">Manager</option>
+	<option value="5">CEO</option>
+</select>
+<br>
+
+SECURITY LEVEL:
+<select name="Sec">
+	<option value="NA">...</option>
+	<option value="1">1</option>
+	<option value="2">2</option>
+	<option value="3">3</option>
+	<option value="4">4</option>
+	<option value="5">5</option>
+</select>
+<br>
+
+CITIZENSHIP:
+<select name="Ctzn">
+	<option value="NA">...</option>
+	<option value="1">Canadian</option>
+	<option value="2">French</option>
+	<option value="3">Mexican</option>
+	<option value="4">Chinese</option>
+	<option value="5">American</option>
+</select>
+<br>
+
+
+<input type="submit" name="Upload" value="UPLOAD!">
+</form>
+
 <?php
+ //REFERENCE: http://www.w3schools.com/php/php_file_upload.asp
 session_start();
 $loc = $_SESSION['loc'];
-$allowedExts = array("gif", "jpeg", "jpg", "png");
-$temp = explode(".", $_FILES["file"]["name"]);
-$extension = end($temp);
 
-if ($_FILES["file"]["size"] < 20000)
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-    }
-  else
-    {
-    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-    echo "Type: " . $_FILES["file"]["type"] . "<br>";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
-
-    if ((file_exists("$loc/" . $_FILES["file"]["name"])) && ($_SESSION['write'] == FALSE))
-      {
-      echo $_FILES["file"]["name"] . " already exists. " . "<br>";
-      }
-    else
-      {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "$loc/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "$loc/" . $_FILES["file"]["name"] . "<br>";
-      }
-    }
-  }
-else
-  {
-  echo "Invalid file";
-  }
-  echo "<a href=dept.php>Departments</a>";
- session_destroy();
-?>
+if(isset($_POST['Upload'])&&isset($_POST['Dept'])&&isset($_POST['Sec'])&&isset($_POST['Role'])&&isset($_POST['Ctzn']))
+	{
+		if($_POST['Dept']=="NA"||$_POST['Sec']=="NA"||$_POST['Role']=="NA"||$_POST['Ctzn']=="NA")
+			echo "Choose an attribute.";
+		else
+		{
+			echo $_POST['Dept']."<br>";
+			echo $_POST['Sec']."<br>";
+			echo $_POST['Role']."<br>";
+			echo $_POST['Ctzn']."<br>";
+			echo $loc."<br>";
+			session_destroy();
+			fileUp($loc);
+		}
+	}
+function fileUp($loc)
+{
+	//I will include the actual file upload code here
+	
+}
