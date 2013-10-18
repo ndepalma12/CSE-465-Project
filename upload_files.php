707945eb9e64e52c7f27a70f59e6501169fd17e2
ndepalma12 <nickolasdepalma@gmail.com>
2013-10-17 19:06:14 -0700

    Update upload_files.php

--- upload_files.php
+++ upload_files.php
@@ -1,7 +1,7 @@
 <body bgcolor = "#00FFFF">
 
 <h2> FILE ATTRIBUTES </h2>	
-<form method="post">
+<form method="post" enctype="multipart/form-data">
 DEPARTMENT:	
 <select name="Dept">
 	<option value="NA">...</option>
@@ -46,32 +46,77 @@ CITIZENSHIP:
 </select>
 <br>
 
+<label for="file">Please Upload File:    </label>
+<input type="file" name="file" id="file"><br>
 
 <input type="submit" name="Upload" value="UPLOAD!">
 </form>
 
 <?php
  //REFERENCE: http://www.w3schools.com/php/php_file_upload.asp
-session_start();
-$loc = $_SESSION['loc'];
+session_start();	//Start session to get location path
+$loc = $_SESSION['loc'];	//Establish location path variable
 
+
+//When Upload is selected and all the attributes are selected
 if(isset($_POST['Upload'])&&isset($_POST['Dept'])&&isset($_POST['Sec'])&&isset($_POST['Role'])&&isset($_POST['Ctzn']))
 	{
-		if($_POST['Dept']=="NA"||$_POST['Sec']=="NA"||$_POST['Role']=="NA"||$_POST['Ctzn']=="NA")
+		if($_POST['Dept']=="NA"||$_POST['Sec']=="NA"||$_POST['Role']=="NA"||$_POST['Ctzn']=="NA")	//Make sure NA isn't chosen
 			echo "Choose an attribute.";
 		else
 		{
+			//These echo lines are just unit testing
+			//We will replace them with SQL Queries setting values in the table using $_POST
 			echo $_POST['Dept']."<br>";
 			echo $_POST['Sec']."<br>";
 			echo $_POST['Role']."<br>";
 			echo $_POST['Ctzn']."<br>";
 			echo $loc."<br>";
-			session_destroy();
-			fileUp($loc);
+			fileUp($loc);	//Call the fileUp function passing the file path
+			session_destroy();	//Destroy the session
 		}
 	}
+
+
+/*****************************************************************************************/
+/****************************************FUNCTIONS****************************************/
+/*****************************************************************************************/
+
+
+
 function fileUp($loc)
 {
+
 	//I will include the actual file upload code here
-	
+	if ($_FILES["file"]["size"] < 20000)	//File size not too big
+	{
+		if ($_FILES["file"]["error"] > 0)
+		{
+			echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+		}
+		else
+		{
+			//Just more echo statements to output file info
+			echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+			echo "Type: " . $_FILES["file"]["type"] . "<br>";
+			echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+			echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+		if ((file_exists("$loc/" . $_FILES["file"]["name"])) && ($_SESSION['write'] == FALSE))	//If the file can be overwritten.  Replace with SQL Query
+		  {
+			echo $_FILES["file"]["name"] . " already exists. " . "<br>";
+		  }
+		else
+		  {
+			move_uploaded_file($_FILES["file"]["tmp_name"],"$loc/" . $_FILES["file"]["name"]);	//Actually upload the file
+			echo "Stored in: " . "$loc/" . $_FILES["file"]["name"] . "<br>";	//Print out file location
+		  }
+		}
+	}
+	else
+	{
+		echo "Invalid file";
+	}
 }
+?>
+<a href="dept.php">DEPARTMENTS</a>
